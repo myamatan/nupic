a05b6049628d2fcef9e26b1507537f587a3c70fb
Masahiro Yamatani <masahiro.yamatani@cern.ch>
2018-06-12 10:04:38 +0200

    merge cpu.py and code.py

--- robot/code.py
+++ robot/code.py
@@ -9,9 +9,29 @@ import soundfile as sf
 import pyaudio as pa
 import cv2
 
+from collections import deque
+import time
+
+import psutil
+import matplotlib.pyplot as plt
+
+from nupic.data.inference_shifter import InferenceShifter
+from nupic.frameworks.opf.model_factory import ModelFactory
+
+import model_params
+
+SECONDS_PER_STEP = 2
+WINDOW = 60
+
+# turn matplotlib interactive mode on (ion)
+plt.ion()
+fig = plt.figure()
+# plot title, legend, etc
+plt.title('CPU prediction example')
+plt.xlabel('time [s]')
 
 # global
-xs = np.array([])
+xs = np.array([0])
 
 
 def callback(in_data, frame_count, time_info, status):
@@ -32,6 +52,23 @@ if __name__ == "__main__":
     chunk = 1024
     use_device_index = 0
 
+    """Poll CPU usage, make predictions, and plot the results. Runs forever."""
+    # Create the model for predicting CPU usage.
+    model = ModelFactory.create(model_params.MODEL_PARAMS)
+    model.enableInference({'predictedField': 'cpu'})
+    # The shifter will align prediction and actual values.
+    shifter = InferenceShifter()
+    # Keep the last WINDOW predicted and actual values for plotting.
+    actHistory = deque([0.0] * WINDOW, maxlen=60)
+    predHistory = deque([0.0] * WINDOW, maxlen=60)
+
+    # Initialize the plot lines that we will update with each new record.
+    actline, = plt.plot(range(WINDOW), actHistory)
+    predline, = plt.plot(range(WINDOW), predHistory)
+    # Set the y-axis range.
+    actline.axes.set_ylim(-10, 10)
+    predline.axes.set_ylim(-10, 10)
+
     in_stream = p_in.open(format=py_format,
                           channels=channels,
                           rate=fs,
@@ -45,14 +82,44 @@ if __name__ == "__main__":
 
     # input loop
     while in_stream.is_active():
-        c = raw_input()
-        if c=='.q':
-            break
+
+        s = time.time()
+
+        # Get the CPU usage.
+        cpu = xs[-1]
+
+        # Run the input through the model and shift the resulting prediction.
+        modelInput = {'cpu': cpu}
+        result = shifter.shift(model.run(modelInput))
+        '''
+
+        # Update the trailing predicted and actual value deques.
+        inference = result.inferences['multiStepBestPredictions'][5]
+        if inference is not None:
+          actHistory.append(result.rawInput['cpu'])
+          predHistory.append(inference)
+
+        # Redraw the chart with the new data.
+        actline.set_ydata(actHistory)  # update the data
+        predline.set_ydata(predHistory)  # update the data
+        plt.draw()
+        plt.legend( ('actual','predicted') )
+
+        # Make sure we wait a total of 2 seconds per iteration.
+        try:
+          plt.pause(SECONDS_PER_STEP)
+        except:
+          pass
+        '''
+
+        print xs[-1], xs.shape
         time.sleep(0.1)
-    else:
-        in_stream.stop_stream()
-        in_stream.close()
+        #c = raw_input()
+        #if c=='.q':
+        #    break
 
-    sf.write("./pyaudio_output.wav", xs, fs)
+    #sf.write("./pyaudio_output.wav", xs, fs)
 
+    in_stream.stop_stream()
+    in_stream.close()
     p_in.terminate()
