6f8a44dd7f677958e6c328f9dfa33fce7d78da99
Masahiro Yamatani <masahiro.yamatani@cern.ch>
2018-06-14 23:15:59 +0200

    add fft.py

--- robot/audio_study/fft.py
+++ robot/audio_study/fft.py
@@ -6,17 +6,13 @@ import sys
 import pyaudio
 import struct
 
+
 class PlotWindow:
 
     def __init__(self):
-        self.win=pg.GraphicsWindow()
-        self.win.setWindowTitle('Realtime plot')
-        self.plt=self.win.addPlot() 
-        self.plt.setYRange(-1,1)   
-        self.curve=self.plt.plot()
-
-        self.CHUNK=1024          
-        self.RATE=16000
+        self.CHUNK=1024   
+        self.RATE=2200
+        self.update_seconds=50
         self.audio=pyaudio.PyAudio()
         self.stream=self.audio.open(format=pyaudio.paInt16,
                                     channels=1,
@@ -24,17 +20,25 @@ class PlotWindow:
                                     input=True,
                                     frames_per_buffer=self.CHUNK)
 
+        self.data=np.zeros(self.CHUNK)
+        self.axis=np.fft.fftfreq(len(self.data), d=1.0/self.RATE)
+
+        self.win=pg.GraphicsWindow()
+        self.win.setWindowTitle("SpectrumAnalyzer")
+        self.plt=self.win.addPlot() 
+        self.plt.setYRange(0,100)  
+
         self.timer=QtCore.QTimer()
         self.timer.timeout.connect(self.update)
-        self.timer.start(50)   
-
-        self.data=np.zeros(self.CHUNK)
+        self.timer.start(self.update_seconds) 
 
     def update(self):
-        self.data=np.append(self.data, self.AudioInput())
-        if len(self.data)/1024 > 5:  
+        self.data=np.append(self.data,self.AudioInput())
+        if len(self.data)/1024 > 10:
             self.data=self.data[1024:]
-        self.curve.setData(self.data)
+        self.fft_data=self.FFT_AMP(self.data)
+        self.axis=np.fft.fftfreq(len(self.data), d=1.0/self.RATE)
+        self.plt.plot(x=self.axis, y=self.fft_data, clear=True, pen="y")  #symbol="o", symbolPen="y", symbolBrush="b")
 
     def AudioInput(self):
         ret=self.stream.read(self.CHUNK)
@@ -43,7 +47,14 @@ class PlotWindow:
         ret[ret <= 0.0] /= float(2**15) 
         return ret
 
+    def FFT_AMP(self, data):
+        data=np.hamming(len(data))*data
+        data=np.fft.fft(data)
+        data=np.abs(data)
+        return data
+
 if __name__=="__main__":
     plotwin=PlotWindow()
     if (sys.flags.interactive!=1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
+
